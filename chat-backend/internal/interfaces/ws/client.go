ced0e2950bc618a0892edd9610655b6ffcdb4291
zhangxing2 <zhangxing2@kingsoft.com>
2026-01-15 15:43:02 +0800

    优化消息

--- chat-backend/internal/interfaces/ws/client.go
+++ chat-backend/internal/interfaces/ws/client.go
@@ -79,19 +79,7 @@ func (c *Client) WritePump() {
 				return
 			}
 
-			w, err := c.Conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			w.Write(message)
-
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write([]byte{'\n'})
-				w.Write(<-c.send)
-			}
-
-			if err := w.Close(); err != nil {
+			if err := c.Conn.WriteMessage(websocket.TextMessage, message); err != nil {
 				return
 			}
 
